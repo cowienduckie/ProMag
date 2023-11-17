7ac98b3645ef6c00953551001cdb0ed823ee77de
Minh Tran <minhtran.2404@outlook.com>
2023-11-17 21:28:08 +0700

    fix: remove global authorization in Master service

--- src/MasterData/MasterData.Api/Extensions.cs
+++ src/MasterData/MasterData.Api/Extensions.cs
@@ -74,11 +74,9 @@ public static class Extensions
             .UseEndpoints(endpoints =>
                 {
                     endpoints.MapGrpcService<MasterDataService>();
-
-                    endpoints
-                        .MapGraphQL()
-                        .RequireAuthorization();
-
+                    
+                    endpoints.MapGraphQL();
+                    
                     endpoints
                         .MapBananaCakePop()
                         .WithOptions(new GraphQLToolOptions
