c58074472c24a9a74c4b91fd711ce6a0e03974ae
Minh Tran <minhtran.2404@outlook.com>
2024-01-11 21:01:05 +0700

    fix: setter for CreatedOn fields

---
+++ src/MasterData/MasterData/Data/Migrations/20240111135718_UpdateMasterData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MasterData.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMasterData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Master",
+                table: "ActivityLogs",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                schema: "Master",
+                table: "ActivityLogs");
+        }
+    }
+}
---
+++ src/PersonalData/PersonalData/Data/Migrations/20240111135404_UpdatePersonalData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalData.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePersonalData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "Workspaces",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                schema: "Personal",
+                table: "Workspaces",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "Teams",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "People",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "Organizations",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "Workspaces");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                schema: "Personal",
+                table: "Workspaces");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                schema: "Personal",
+                table: "Organizations");
+        }
+    }
+}
